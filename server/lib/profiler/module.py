9d582c46c66d57ff1fc944ed77feb9907861fde2
Geoffrey Yu <geoffxy@gmail.com>
2019-03-22 11:37:13 -0400

    Handle errors that may arise from layer profiling

--- server/lib/profiler/module.py
+++ server/lib/profiler/module.py
@@ -2,29 +2,34 @@ import torch
 import logging
 
 from lib.config import Config
+from lib.exceptions import AnalysisError
 
 logger = logging.getLogger(__name__)
 
 
 def get_operation_runtimes(model, annotation_info, model_operations):
-    # 1. Create CUDA events used for timing
-    start_event = torch.cuda.Event(enable_timing=True)
-    end_event = torch.cuda.Event(enable_timing=True)
+    try:
+        # 1. Create CUDA events used for timing
+        start_event = torch.cuda.Event(enable_timing=True)
+        end_event = torch.cuda.Event(enable_timing=True)
 
-    # 2. Attach profiling hooks to each relevant module
-    for name, module in model.named_modules():
-        operation_info = model_operations.get_operation_info_by_bound_name(
-            name)
-        if operation_info is None:
-            continue
+        # 2. Attach profiling hooks to each relevant module
+        for name, module in model.named_modules():
+            operation_info = model_operations.get_operation_info_by_bound_name(
+                name)
+            if operation_info is None:
+                continue
 
-        module.register_forward_hook(
-            _make_profiling_hook(operation_info, (start_event, end_event)))
+            module.register_forward_hook(
+                _make_profiling_hook(operation_info, (start_event, end_event)))
 
-    # 3. Perform one forward pass to trigger the profiling
-    model_input = torch.randn(
-        annotation_info.input_size, device=torch.device('cuda'))
-    output = model(model_input)
+        # 3. Perform one forward pass to trigger the profiling
+        model_input = torch.randn(
+            annotation_info.input_size, device=torch.device('cuda'))
+        output = model(model_input)
+
+    except Exception as ex:
+        raise AnalysisError(str(ex), type(ex))
 
 
 def _make_profiling_hook(operation_info, events):
