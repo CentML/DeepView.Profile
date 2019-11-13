986a9bebbcb7ebc99d8634829ef7bc9102b98546
Geoffrey Yu <geoffxy@gmail.com>
2019-11-13 18:06:08 -0500

    Add initial experimental iteration memory tracker

--- experimental/memory/tracker/_iteration.py
+++ experimental/memory/tracker/_iteration.py
@@ -1,3 +1,6 @@
+import torch
+import inspect
+
 from tracker._base import _TrackerBase
 from tracker.meters import MemoryMeter
 from hook_manager import HookManager
@@ -12,9 +15,43 @@ class _IterationTracker(_TrackerBase):
     def start_tracking(self):
         super().start_tracking()
         self._meter.reset()
+        self._hook_manager.attach_hooks_on_module(
+            torch,
+            self._is_callable,
+            self._callable_hook_creator,
+        )
+        self._hook_manager.attach_hooks_on_module(
+            torch.Tensor,
+            self._is_callable,
+            self._callable_hook_creator,
+        )
+        self._hook_manager.attach_hooks_on_module(
+            torch.nn.functional,
+            self._is_callable,
+            self._callable_hook_creator,
+        )
 
     def stop_tracking(self):
         super().stop_tracking()
+        self._hook_manager.remove_hooks()
+
+    def _callable_hook_creator(self, func):
+        def hook(*args, **kwargs):
+            self._meter.checkpoint()
+            retval = func(*args, **kwargs)
+            delta = self._meter.checkpoint()
+            if delta > 0 or delta < 0:
+                print(func.__name__, delta)
+            return retval
+        return hook
+
+    def _is_callable(self, maybe_fn):
+        return (
+            inspect.isfunction(maybe_fn) or
+            inspect.ismethod(maybe_fn) or
+            inspect.isbuiltin(maybe_fn) or
+            inspect.isroutine(maybe_fn)
+        )
 
     def get_report(self):
         return []
