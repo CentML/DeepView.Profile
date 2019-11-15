43a0d71455dc6f94e012482a86f9df7d4046c601
Geoffrey Yu <geoffxy@gmail.com>
2019-11-15 17:13:49 -0500

    Update iteration tracking to use gradient functions

--- experimental/memory/tracker/_iteration.py
+++ experimental/memory/tracker/_iteration.py
@@ -1,9 +1,17 @@
 import torch
 import inspect
+import collections
 
 from tracker._base import _TrackerBase
+from tracker.call_stack import CallStack
 from tracker.meters import MemoryMeter
 from hook_manager import HookManager
+
+
+OperationContext = collections.namedtuple(
+    'OperationContext',
+    ['operation_name', 'stack'],
+)
 
 
 class _IterationTracker(_TrackerBase):
@@ -11,10 +19,12 @@ class _IterationTracker(_TrackerBase):
         super().__init__()
         self._hook_manager = HookManager()
         self._meter = MemoryMeter()
+        self._grad_function_contexts = {}
 
     def start_tracking(self):
         super().start_tracking()
         self._meter.reset()
+        self._grad_function_contexts.clear()
         self._hook_manager.attach_hooks_on_module(
             torch,
             self._is_callable,
@@ -45,13 +55,25 @@ class _IterationTracker(_TrackerBase):
 
     def _callable_hook_creator(self, func):
         def hook(*args, **kwargs):
-            self._meter.checkpoint()
             retval = func(*args, **kwargs)
-            delta = self._meter.checkpoint()
-            if delta > 0:
-                #print(func.__name__, delta, type(retval))
-                pass
+
+            # Early return for tensor-producing operations that are not
+            # involved in the backward pass
+            if (not isinstance(retval, torch.Tensor) and
+                    not isinstance(retval, tuple) and
+                    not isinstance(retval, list)):
+                return retval
+            if (isinstance(retval, torch.Tensor) and
+                    (not retval.is_cuda or retval.grad_fn is None)):
+                return retval
+
+            context = OperationContext(
+                operation_name=func.__name__,
+                stack=CallStack.from_here(start_from=2),
+            )
+            self._handle_callable_result(retval, context)
             return retval
+
         return hook
 
     def _backward_hook_creator(self, func):
@@ -59,6 +81,14 @@ class _IterationTracker(_TrackerBase):
             print('Tensor.backward() called')
             return func(*args, **kwargs)
         return hook
+
+    def _handle_callable_result(self, retval, context):
+        if isinstance(retval, torch.Tensor) and retval.grad_fn is not None:
+            self._grad_function_contexts[retval.grad_fn] = context
+
+        elif isinstance(retval, tuple) or isinstance(retval, list):
+            for inner_value in retval:
+                self._handle_callable_result(inner_value, context)
 
     def _is_callable(self, maybe_fn):
         is_callable = (
