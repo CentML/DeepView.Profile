b4f0bf0ce55b09c3e22cb2c02ae77d445c1d6016
Geoffrey Yu <geoffxy@gmail.com>
2019-12-06 13:32:59 -0500

    [Server] Request and validate project entry point on server start

--- cli/innpv/interactive.py
+++ cli/innpv/interactive.py
@@ -1,13 +1,21 @@
 import logging
+import os
 import signal
 import sys
 import threading
+
+logger = logging.getLogger(__name__)
 
 
 def register_command(subparsers):
     parser = subparsers.add_parser(
         "interactive",
         help="Start a new INNPV interactive profiling session.",
+    )
+    parser.add_argument(
+        "entry_point",
+        help="The entry point file in this project that contains the INNPV "
+             "provider functions.",
     )
     parser.add_argument(
         "--host",
@@ -54,6 +62,37 @@ def configure_logging(args):
     logging.basicConfig(**kwargs)
 
 
+def validate_paths(project_root, entry_point):
+    if not os.path.isabs(project_root):
+        logger.error(
+            "The project root that INNPV received is not an absolute path. "
+            "This is an unexpected error. Please report a bug."
+        )
+        logger.error("Current project root: %s", project_root)
+        return False
+
+    if os.path.isabs(entry_point):
+        logger.error(
+            "The entry point must be specified as a relative path to the "
+            "current directory. Please double check that the entry point you "
+            "are providing is a relative path.",
+        )
+        logger.error("Current entry point path: %s", entry_point)
+        return False
+
+    full_path = os.path.join(project_root, entry_point)
+    if not os.path.isfile(full_path):
+        logger.error(
+            "Either the specified entry point is not a file or its path was "
+            "specified incorrectly. Please double check that it exists and "
+            "that its path is correct.",
+        )
+        logger.error("Current absolute path to entry point: %s", full_path)
+        return False
+
+    return True
+
+
 def main(args):
     from innpv.config import Config
     from innpv.server import INNPVServer
@@ -67,14 +106,24 @@ def main(args):
     signal.signal(signal.SIGTERM, signal_handler)
 
     configure_logging(args)
+
+    project_root = os.getcwd()
+    entry_point = args.entry_point
+    if not validate_paths(project_root, entry_point):
+        sys.exit(1)
+
     Config.parse_args(args)
 
-    with INNPVServer(args.host, args.port) as server:
+    with INNPVServer(
+            args.host, args.port, project_root, entry_point) as server:
         _, port = server.listening_on
-        logging.info(
+        logger.info(
             "INNPV interactive profiling session started! "
             "Listening on port %d.",
             port,
         )
+        logger.info("Project Root (server):  %s", project_root)
+        logger.info("Entry Point:            %s", entry_point)
+
         # Run the server until asked to terminate
         should_shutdown.wait()
--- cli/innpv/server.py
+++ cli/innpv/server.py
@@ -11,10 +11,13 @@ logger = logging.getLogger(__name__)
 
 
 class INNPVServer:
-    def __init__(self, host, port):
+    def __init__(self, host, port, project_root, entry_point):
         self._requested_host = host
         # This is the port the user specified on the command line (it can be 0)
         self._requested_port = port
+        self._project_root = project_root
+        self._entry_point = entry_point
+
         self._connection_acceptor = ConnectionAcceptor(
             self._requested_host,
             self._requested_port,
