e52e9d10c8a3a0a62f86f9da93d33e9672a88e02
Geoffrey Yu <geoffxy@gmail.com>
2019-12-13 16:48:34 -0500

    [Plugin] Add ability to add labels to the PerfBarContainer

--- plugin/lib/components/generic/PerfBarContainer.js
+++ plugin/lib/components/generic/PerfBarContainer.js
@@ -16,7 +16,7 @@ class PerfBarContainer extends React.Component {
   }
 
   _classes() {
-    const mainClass = 'innpv-perfbarcontainer';
+    const mainClass = 'innpv-perfbarcontainer-wrap';
     if (this.props.disabled) {
       return mainClass + ' innpv-no-events';
     }
@@ -26,17 +26,20 @@ class PerfBarContainer extends React.Component {
   render() {
     return (
       <div className={this._classes()}>
-        <div
-          className="innpv-perfbarcontainer-inner"
-          style={{marginTop: `-${this.state.marginTop}px`}}
-        >
-          {this.props.data.map(
-            (dataElement, index) => this.props.perfBarGenerator(
-              dataElement,
-              index,
-              this._updateMarginTop,
-            ))}
+        <div className="innpv-perfbarcontainer">
+          <div
+            className="innpv-perfbarcontainer-inner"
+            style={{marginTop: `-${this.state.marginTop}px`}}
+          >
+            {this.props.data.map(
+              (dataElement, index) => this.props.perfBarGenerator(
+                dataElement,
+                index,
+                this._updateMarginTop,
+              ))}
+          </div>
         </div>
+        <LabelContainer labels={this.props.labels} />
       </div>
     );
   }
@@ -46,6 +49,23 @@ PerfBarContainer.defaultProps = {
   data: [],
   perfBarGenerator: (dataElement, index, updateMarginTop) => null,
   disabled: false,
+  labels: [],
 };
+
+function LabelContainer(props) {
+  if (props.labels.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="innpv-perfbarcontainer-labelcontainer">
+      {props.labels.map(({label, percentage}) => (
+        <div className="innpv-perfbarcontainer-labelwrap" style={{height: `${percentage}%`}}>
+          {percentage >= 10 ? <div className="innpv-perfbarcontainer-label">{label}</div> : null}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default PerfBarContainer;
