9424f170fc665eeecccd796b9227a0f4751ec921
Geoffrey Yu <geoffxy@gmail.com>
2019-12-24 12:10:30 -0500

    [Server] Set REUSEADDR on the server socket

--- cli/innpv/io/connection_acceptor.py
+++ cli/innpv/io/connection_acceptor.py
@@ -24,6 +24,11 @@ class ConnectionAcceptor:
             socket.AF_INET,
             socket.SOCK_STREAM,
         )
+        self._server_socket.setsockopt(
+            socket.SOL_SOCKET,
+            socket.SO_REUSEADDR,
+            1,
+        )
         self._handler_function = handler_function
         self._acceptor = Thread(target=self._accept_connections)
         self._sentinel = Sentinel()
