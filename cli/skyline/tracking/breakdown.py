67fa5f9cd672233980b70f5e4af943872981d874
Geoffrey Yu <geoffxy@gmail.com>
2020-02-09 11:50:38 -0500

    [Server] Bug fix: Avoid including current node's contexts in the aggregate context info map

--- cli/skyline/tracking/breakdown.py
+++ cli/skyline/tracking/breakdown.py
@@ -223,20 +223,25 @@ class OperationNode(BreakdownNode):
 
             if visit_count > 0:
                 for child in node.children.values():
+                    context_info = ContextInfo(
+                        size_bytes=child.size_bytes,
+                        run_time_ms=(
+                            child.forward_ms if child.backward_ms is None
+                            else child.forward_ms + child.backward_ms
+                        ),
+                    )
+                    for context in child._contexts:
+                        if context in node._context_info_map:
+                            node._context_info_map[context] += context_info
+                        else:
+                            node._context_info_map[context] = context_info
+
                     ContextInfo.merge_map(
                         node._context_info_map,
                         child._context_info_map,
                     )
             else:
                 node._context_info_map = {}
-                for context in node._contexts:
-                    node._context_info_map[context] = ContextInfo(
-                        size_bytes=node.size_bytes,
-                        run_time_ms=(
-                            node.forward_ms if node.backward_ms is None
-                            else node.forward_ms + node.backward_ms
-                        ),
-                    )
                 stack.append((node, 1))
                 for child in node.children.values():
                     stack.append((child, 0))
