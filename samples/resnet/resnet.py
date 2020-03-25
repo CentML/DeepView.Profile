252acb30acc8ae673a59ea8e0d25bb6b014d6af3
Geoffrey Yu <geoffxy@gmail.com>
2020-03-25 18:28:10 -0400

    [Samples] Add ResNeXt as a sample model

--- samples/resnet/resnet.py
+++ samples/resnet/resnet.py
@@ -216,3 +216,12 @@ def resnet50(**kwargs):
     `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
     """
     return _resnet('resnet50', Bottleneck, [3, 4, 6, 3], **kwargs)
+
+
+def resnext50_32x4d(**kwargs):
+    r"""ResNeXt-50 32x4d model from
+    `"Aggregated Residual Transformation for Deep Neural Networks" <https://arxiv.org/pdf/1611.05431.pdf>`_
+    """
+    kwargs['groups'] = 32
+    kwargs['width_per_group'] = 4
+    return _resnet('resnext50_32x4d', Bottleneck, [3, 4, 6, 3], **kwargs)
