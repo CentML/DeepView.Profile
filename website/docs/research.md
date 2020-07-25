5acfebb9c26c976d72591ca77da93ad33574ff54
Geoffrey Yu <geoffxy@gmail.com>
2020-07-24 23:06:01 -0400

    [Website] Remove duplicated citation block component

--- website/docs/research.md
+++ website/docs/research.md
@@ -12,38 +12,6 @@ User Interface Software and Technology
 If you use Skyline in your research and find it useful, please consider citing
 our paper.
 
-import Tabs from '@theme/Tabs';
-import TabItem from '@theme/TabItem';
+import CitationBlock from '../src/components/CitationBlock';
 
-<Tabs
-  defaultValue="bibtex"
-  values={[
-    { label: 'BibTeX', value: 'bibtex', },
-    { label: 'Plain Text', value: 'txt', },
-  ]
-}>
-<TabItem value="bibtex">
-
-```latex
-@inproceedings{skyline-yu20,
-  title = {{Skyline: Interactive In-editor Computational Performance Profiling
-    for Deep Neural Network Training}},
-  author = {Yu, Geoffrey X. and Grossman, Tovi and Pekhimenko, Gennady},
-  booktitle = {{Proceedings of the 33rd ACM Symposium on User Interface
-    Software and Technology (UIST'20)}},
-  year = {2020},
-}
-```
-
-</TabItem>
-<TabItem value="txt">
-
-```txt
-Geoffrey X. Yu, Tovi Grossman, Gennady Pekhimenko. Skyline: Interactive
-In-editor Computational Performance Profiling for Deep Neural Network Training.
-In Proceedings of the 33rd ACM Symposium on User Interface Software and
-Technology (UIST'20). 2020.
-```
-
-</TabItem>
-</Tabs>
+<CitationBlock />
--- website/src/components/PaperDetails.js
+++ website/src/components/PaperDetails.js
@@ -3,10 +3,7 @@ import clsx from 'clsx';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 import VideoOverlay from './VideoOverlay';
-
-import CodeBlock from '@theme/CodeBlock';
-import Tabs from '@theme/Tabs';
-import TabItem from '@theme/TabItem';
+import CitationBlock from './CitationBlock';
 
 class PaperDetails extends React.Component {
   constructor(props) {
@@ -54,20 +51,6 @@ class PaperDetails extends React.Component {
   }
 }
 
-const bibtexCitation = `@inproceedings{skyline-yu20,
-  title = {{Skyline: Interactive In-editor Computational Performance Profiling
-    for Deep Neural Network Training}},
-  author = {Yu, Geoffrey X. and Grossman, Tovi and Pekhimenko, Gennady},
-  booktitle = {{Proceedings of the 33rd ACM Symposium on User Interface
-    Software and Technology (UIST'20)}},
-  year = {2020},
-}`;
-
-const textCitation = `Geoffrey X. Yu, Tovi Grossman, Gennady Pekhimenko.
-Skyline: Interactive In-editor Computational Performance Profiling for
-Deep Neural Network Training. In Proceedings of the 33rd ACM Symposium
-on User Interface Software and Technology (UIST'20). 2020.`;
-
 function ViewPreprint() {
   return (
     <a
@@ -77,27 +60,6 @@ function ViewPreprint() {
     >
       View Preprint
     </a>
-  );
-}
-
-function CitationBlock() {
-  return (
-    <div className={styles.citationBlock}>
-      <Tabs
-        defaultValue="bibtex"
-        values={[
-          { label: 'BibTeX', value: 'bibtex', },
-          { label: 'Plain Text', value: 'txt', },
-        ]}
-      >
-        <TabItem value="bibtex">
-          <CodeBlock language="tex">{bibtexCitation}</CodeBlock>
-        </TabItem>
-        <TabItem value="txt">
-          <CodeBlock language="txt">{textCitation}</CodeBlock>
-        </TabItem>
-      </Tabs>
-    </div>
   );
 }
 
