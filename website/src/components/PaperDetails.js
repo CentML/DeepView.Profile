93ae112e63232a63286efcb74723f141118ab91d
Geoffrey Yu <geoffxy@gmail.com>
2020-07-24 23:00:14 -0400

    [Website] Implement VideoOverlay component

--- website/src/components/PaperDetails.js
+++ website/src/components/PaperDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
+import VideoOverlay from './VideoOverlay';
 
 import CodeBlock from '@theme/CodeBlock';
 import Tabs from '@theme/Tabs';
@@ -12,21 +13,30 @@ class PaperDetails extends React.Component {
     super(props);
     this.state = {
       showCitation: false,
+      showVideo: false,
     };
     this._showCitationClick = this._showCitationClick.bind(this);
+    this._toggleVideo = this._toggleVideo.bind(this);
   }
 
   _showCitationClick() {
     this.setState({showCitation: !this.state.showCitation});
   }
 
+  _toggleVideo() {
+    this.setState({showVideo: !this.state.showVideo});
+  }
+
   render() {
-    const {showCitation} = this.state;
+    const {showCitation, showVideo} = this.state;
     return (
       <div className={clsx('container', styles.paperDetails)}>
         <ViewPreprint />
-        <button className="button button--secondary detailsButton">
-          See Video
+        <button
+          onClick={this._toggleVideo}
+          className="button button--secondary detailsButton"
+        >
+          Watch the Video
         </button>
         <button
           onClick={this._showCitationClick}
@@ -38,6 +48,7 @@ class PaperDetails extends React.Component {
           {showCitation ? 'Hide Citation' : 'Show Citation'}
         </button>
         {showCitation ? <CitationBlock /> : null}
+        {showVideo ? <VideoOverlay onCloseClick={this._toggleVideo} /> : null}
       </div>
     );
   }
