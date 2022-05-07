de25e9a97f84f50fcb68a4da5467d6789dd2642f
Akbar Nurlybayev <akbar.nur@gmail.com>
2022-05-06 22:23:16 -0500

    Add prepare release script

---
+++ tools/common.sh
@@ -0,0 +1,110 @@
+function pushd() {
+    command pushd "$@" > /dev/null
+}
+
+function popd() {
+    command popd "$@" > /dev/null
+}
+
+COLOR_RED="\033[0;31m"
+COLOR_GREEN="\033[0;32m"
+COLOR_YELLOW="\033[0;33m"
+COLOR_BLUE="\033[0;36m"
+COLOR_NC="\033[0m"
+
+function echo_colored() {
+  echo -e "${1}${2}${COLOR_NC}"
+}
+
+function echo_green() {
+  echo_colored "$COLOR_GREEN" "$1"
+}
+
+function echo_red() {
+  echo_colored "$COLOR_RED" "$1"
+}
+
+function echo_yellow() {
+  echo_colored "$COLOR_YELLOW" "$1"
+}
+
+function echo_blue() {
+  echo_colored "$COLOR_BLUE" "$1"
+}
+
+function prompt_yn() {
+  echo -en "${COLOR_YELLOW}$1${COLOR_NC}"
+  read -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+    exit 1
+  fi
+}
+
+function get_repo_hash() {
+  echo "$(git rev-parse --short HEAD)"
+}
+
+function check_repo() {
+  # Make sure no unstaged changes
+  echo_yellow "> Check for uncommitted changes"
+  if [[ ! -z $(git status --porcelain) ]];
+  then
+    echo_red "ERROR: There are uncommitted changes. Please commit before releasing."
+    exit 1
+  fi
+
+  # Make sure we're on main
+  echo_yellow "> Check the current branch"
+  INNPV_MAIN_HASH=$(git rev-parse main)
+  INNPV_HASH=$(git rev-parse HEAD)
+
+  if [[ $INNPV_MAIN_HASH != $INNPV_HASH ]]; then
+    echo_red "ERROR: You must be on main when releasing."
+    exit 1
+  fi
+
+  INNPV_SHORT_HASH=$(git rev-parse --short HEAD)
+
+  echo_green "✓ Repository OK"
+}
+
+function check_tools() {
+  echo_yellow "> Check tools"
+  if [ -z "$(which poetry)" ]; then
+    echo_red "ERROR: Poetry must be installed."
+    exit 1
+  fi
+
+  echo_green "✓ Release tooling OK"
+}
+
+function build_release() {
+  # pushd ../cli
+
+  echo_yellow "> Building wheels..."
+  rm -rf dist/*
+  # cp pyproject.toml skyline/
+  poetry build
+  echo_green "✓ Wheels successfully built"
+
+  # popd
+}
+
+function upload_release() {
+  pushd ../cli
+
+  echo ""
+  echo_yellow "> Uploading release to PyPI..."
+  twine upload -r pypi "dist/skyline_cli-${NEXT_CLI_VERSION}*"
+  echo_green "✓ New release uploaded to PyPI"
+
+  echo ""
+  echo_yellow "> Creating a release tag..."
+  git tag -a "$VERSION_TAG" -m ""
+  git push --follow-tags
+  echo_green "✓ Git release tag created and pushed to GitHub"
+
+  popd
+}
+
---
+++ tools/prepare-release.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# This script is used to release a new version of the Skyline CLI.
+# Release steps:
+# 1. Create release branch
+# 2. Increment package version in pyproject.toml
+# 3. Prepare change log since the last version 
+# 4. Commit the change log
+# 5. Creater draft Github release
+# 6. Optional: create ability to publish to test pypi
+
+
+set -e
+
+RELEASE_SCRIPT_PATH=$(cd $(dirname $0) && pwd -P)
+cd $RELEASE_SCRIPT_PATH
+source common.sh
+
+echo ""
+echo_blue "Skyline Profiler Release Preparation Tool"
+echo_blue "========================================="
+
+echo ""
+check_repo
+
+echo ""
+check_tools
+
+echo ""
+echo_yellow "> Tooling versions:"
+echo "$(poetry --version)"
+echo "$(poetry run python3 --version)"
+
+CURR_CLI_VERSION=$(poetry version --short)
+echo -en "${COLOR_YELLOW}Release increment: [patch], minor, major${COLOR_NC}"
+read -r
+case $REPLY in 
+major)
+  poetry version major;;
+minor)
+  poetry version minor;;
+*)
+  poetry version patch;;
+esac
+NEXT_CLI_VERSION=$(poetry version --short)
+VERSION_TAG="v$NEXT_CLI_VERSION"
+REPO_HASH=$(get_repo_hash)
+
+echo ""
+echo_yellow "> The next CLI version will be '$VERSION_TAG'."
+prompt_yn "> Is this correct? (y/N) "
+
+echo ""
+build_release
+
+case $1 in
+--deploy)
+echo ""
+prompt_yn "> Ready to release to PyPI? (y/N) "
+
+echo ""
+echo_yellow "> Releasing $VERSION_TAG of the CLI...";;
+--test-deploy)
+echo ""
+prompt_yn "> Ready to release to Test PyPI? (y/N) "
+
+echo ""
+echo_yellow "> Releasing $VERSION_TAG of the CLI...";;
+# upload_release
+*)
+echo ""
+echo_yellow "Skipping the upload to PyPI neither --deploy nor --test-deploy was passed."
+echo_green "✓ Done!"
+exit 0;;
+esac
+
+echo_green "✓ Done!"
+
