50d9b1ce100c5a12f16682cca2e2ba861afaad5a
michaelshin <michaelshin@users.noreply.github.com>
2023-04-18 16:30:59 -0400

    Add batch size to energy response

--- skyline/db/database.py
+++ skyline/db/database.py
@@ -5,7 +5,6 @@ import sqlite3
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DB_PATH = os.path.join(BASE_DIR, "deepview.sqlite3")
 
-
 class DatabaseInterface:
     def __init__(self, database_name=DB_PATH) -> None:
         self.connection = sqlite3.connect(database_name, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
@@ -16,6 +15,7 @@ class DatabaseInterface:
             entry_point TEXT, \
             cpu_component REAL, \
             gpu_component REAL, \
+            batch_size INT, \
             ts TIMESTAMP \
         );")
 
@@ -31,8 +31,8 @@ class EnergyTableInterface:
         and the types match the columns. Note that timestamp is not part of the entry.
         Returns True if it is valid, else False
         '''
-        return len(entry) == 3 and type(entry[0]) == str and type(entry[1]) == float \
-            and type(entry[2]) == float
+        return len(entry) == 4 and type(entry[0]) == str and type(entry[1]) == float \
+            and type(entry[2]) == float and type(entry[3]) == int
 
     @staticmethod
     def is_valid_entry_with_timestamp(entry: list) -> bool:
@@ -40,8 +40,9 @@ class EnergyTableInterface:
         Validates an entry in the Energy table by testing if the length is 4,
         and the types match the columns. Returns True if it is valid, else False
         '''
-        return len(entry) == 4 and type(entry[0]) == str and type(entry[1]) == float \
-            and type(entry[2]) == float and type(entry[3]) == datetime.datetime
+        return len(entry) == 5 and type(entry[0]) == str and type(entry[1]) == float \
+            and type(entry[2]) == float and type(entry[3]) == int \
+            and type(entry[4]) == datetime.datetime
 
     def add_entry(self, entry: list) -> bool:
         '''
@@ -53,14 +54,14 @@ class EnergyTableInterface:
             try:
                 entry.append(datetime.datetime.now())
                 cursor = self.database_connection.cursor()
-                cursor.execute("INSERT INTO ENERGY VALUES(?, ?, ?, ?)", entry)
+                cursor.execute("INSERT INTO ENERGY VALUES(?, ?, ?, ?, ?)", entry)
                 self.database_connection.commit()
                 return True
             except sqlite3.IntegrityError as e:
                 print(e)
                 return False
         else:
-            return True
+            return False
 
     def get_latest_n_entries_of_entry_point(self, n: int, entry_point: str) -> list:
         '''
