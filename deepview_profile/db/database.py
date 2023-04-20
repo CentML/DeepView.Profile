c3ff263140e6366e5d9182df39e1298a0794b81d
Michael Shin <michaelshin@users.noreply.github.com>
2023-04-20 15:55:41 -0400

    Fix missing bracket (#47)
    * Fix missing bracket
    
    * Update version number

--- deepview_profile/db/database.py
+++ deepview_profile/db/database.py
@@ -2,7 +2,7 @@ import datetime
 import os
 import sqlite3
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DB_PATH = os.path.join(BASE_DIR, "deepview.sqlite3")
 
 class DatabaseInterface:
