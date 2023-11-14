41f56f8d4951ad563cf1968fd0dc5533112e023a
Oleg V Kolesnikov <okolesnikov@itera-research.com>
2023-11-14 14:23:51 +0200

    Added analysis command and json output

--- deepview_profile/utils.py
+++ deepview_profile/utils.py
@@ -2,9 +2,15 @@ import torch
 import logging
 import gc
 
+from google.protobuf.json_format import MessageToDict
+
 logger = logging.getLogger(__name__)
 
 def release_memory():
     logger.debug("Emptying cache")
     gc.collect()
     torch.cuda.empty_cache()
+
+def next_message_to_dict(object): 
+    message = next(object)
+    return MessageToDict(message)
