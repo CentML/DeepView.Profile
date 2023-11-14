74618c6009d4dc76e6dfd18e82d6712533653a43
Oleg V Kolesnikov <okolesnikov@itera-research.com>
2023-11-14 14:06:52 +0200

    Improved NVML

--- deepview_profile/nvml.py
+++ deepview_profile/nvml.py
@@ -23,7 +23,7 @@ class NVML:
     def get_device_names(self):
         device_names = []
         for i in range(pynvml.nvmlDeviceGetCount()):
-            handle = pynvml.nvmlDeviceGetHandleByIndex()
+            handle = pynvml.nvmlDeviceGetHandleByIndex(i)
             device_name = pynvml.nvmlDeviceGetName(handle).decode("utf-8")
             device_names.append(device_name)
         return device_names
