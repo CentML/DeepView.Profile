5146638836770ff1157043f31e6334e252e388c7
Oleg V Kolesnikov <okolesnikov@itera-research.com>
2023-11-20 21:17:02 +0200

    Improved utils

--- deepview_profile/utils.py
+++ deepview_profile/utils.py
@@ -45,6 +45,7 @@ def encode_file(root, file):
         with open(filename, "r") as f:
             file_content = f.read()
             file_dict["content"] = base64.b64encode(file_content.encode("utf-8")).decode("utf-8")
+
     return file_dict
 
 
