82511815f9ca7843b7407fc8e85428380372ed0a
John Calderon <john.calderon@centml.ai>
2024-02-28 18:23:51 -0500

    changed utils.py

--- test/utils.py
+++ test/utils.py
@@ -1,4 +1,5 @@
 import os
+import select
 import socket
 import struct
 import subprocess
@@ -17,27 +18,47 @@ def stream_monitor(stream, callback=None):
 
 def socket_monitor(socket, callback=None):
     try:
-        while True:
-            msg_len = struct.unpack(">I", socket.recv(4))[0]
-            msg = socket.recv(msg_len)
-            print(f"Received message of length {msg_len}")
-            callback(msg)
-    except OSError:
+        while socket:
+            ret = select.select([socket], [], [], 1)
+            if len(ret[0]):
+                msg_len = struct.unpack(">I", socket.recv(4))[0]
+                buf = None
+                while socket and msg_len:
+                    msg = socket.recv(msg_len)
+                    if len(msg) == 0: break
+                    buf = buf + msg if buf else msg
+                    msg_len -= len(msg)
+
+                callback(buf)
+    except (ValueError, OSError):
+        # terminate whenever either recv() or select() fails.
+        # - if the socket is closed, then recv() will raise OSError as it attempts to read
+        #   from a closed file descriptor.
+        # - if the socket is closed, then select() will receive an invalid (i.e. negative) file descriptor
+        #   and will raise a ValueError
         print(f"Closing listener for socket {socket}")
 
 
 class BackendContext:
-    def __init__(self, skyline_bin, entry_point):
+    def __init__(self, entry_point, stdout_fd=None, stderr_fd=None):
         self.process = None
-        self.skyline_bin = skyline_bin
         self.entry_point = entry_point
         self.state = 0
+        self.stdout_fd = stdout_fd
+        self.stderr_fd = stderr_fd
 
     def on_message_stdout(self, message):
-        message = message.decode("ascii").rstrip()
+        # message = message.decode("ascii").rstrip()
+        message = message.decode("ascii")
+        if self.stdout_fd:
+            self.stdout_fd.write(message)
 
     def on_message_stderr(self, message):
-        message = message.decode("ascii").rstrip()
+        message = message.decode("ascii")
+        if self.stderr_fd:
+            self.stderr_fd.write(message)
+
+        message = message.rstrip()
         print("stderr", message)
         if "DeepView interactive profiling session started!" in message:
             self.state = 1
@@ -46,8 +67,7 @@ class BackendContext:
         # DeepView expects the entry_point filename to be relative
         working_dir = os.path.dirname(self.entry_point)
         entry_filename = os.path.basename(self.entry_point)
-        launch_command = [self.skyline_bin, "interactive", entry_filename]
-
+        launch_command = ["python", "-m", "deepview_profile", "interactive", "--debug"]
         # Launch backend + listener threads for stdout and stderr
         self.process = subprocess.Popen(
             launch_command,
@@ -64,6 +84,9 @@ class BackendContext:
         )
         self.stderr_thread.start()
 
+    def alive(self):
+        return self.process and self.process.poll() is None
+
     def join(self):
         self.process.wait()
 
@@ -73,7 +96,7 @@ class BackendContext:
         self.stderr_thread.join()
 
 
-class SkylineSession:
+class DeepviewSession:
     def __init__(self):
         self.seq_num = 0
         self.received_messages = []
@@ -103,9 +126,11 @@ class SkylineSession:
         self.socket.sendall(length_buffer)
         self.socket.sendall(buf)
 
-    def send_initialize_request(self):
+    def send_initialize_request(self, project_root):
         request = innpv_pb2.InitializeRequest()
         request.protocol_version = 5
+        request.project_root = project_root
+        request.entry_point = "entry_point.py"
         self.send_message(request)
 
     def send_analysis_request(self):
@@ -116,11 +141,16 @@ class SkylineSession:
     def handle_message(self, message):
         from_server = innpv_pb2.FromServer()
         from_server.ParseFromString(message)
-        # print("From Server:")
-        # print(from_server)
+        print("From Server:")
+        print(from_server)
 
         self.received_messages.append(from_server)
         print(f"new message. total: {len(self.received_messages)}")
+
+    def alive(self):
+        # makes sure that the last message is not error
+        return (not self.received_messages) or \
+            not self.received_messages[-1].HasField("analysis_error")
 
     def cleanup(self):
         # Closing the socket should cause the listener thread to die
