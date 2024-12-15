65c68304cab3d3311f077d6d3d83f9543d656b30
Yu Bo Gao <ybgao@cs.toronto.edu>
2024-12-14 21:44:20 -0500

    Allow unassociated weight during trace; Instead of terminating with an error only raise a warning

--- deepview_profile/tracking/breakdown.py
+++ deepview_profile/tracking/breakdown.py
@@ -1,6 +1,7 @@
 import collections
 import math
 import os
+import warnings
 
 HierarchicalBreakdown = collections.namedtuple(
     'HierarchicalBreakdown', ['operations', 'weights', 'peak_usage_bytes'])
@@ -48,8 +49,7 @@ class HierarchicalBreakdownBuilder:
     def add_weight_entry(
             self, weight_name, size_bytes, grad_size_bytes, stack_context):
         if len(stack_context.frames) == 0:
-            raise ValueError(
-                'Adding weight entry with no context to the breakdown.')
+            warnings.warn(f"{weight_name} has no accociated context")
 
         for entry in self._traverse_and_insert(
                 self._weight_root, weight_name, stack_context):
